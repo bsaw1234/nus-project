3475111be7f1f1f1ea485c3cd7c250eff4857a04
bsaw1234 <102780871+bsaw1234@users.noreply.github.com>
2022-04-08 23:12:03 +0800

    Update read_stock_data.py

--- read_stock_data.py
+++ read_stock_data.py
@@ -14,12 +14,20 @@ import csv
 #f = open('C:/Users/baska/Downloads/NUS/Big Data/Projects/stock_data.csv', 'w')
 
 # Read and print the stock tickers that make up S&P500
-tickers = pd.read_html(
-    'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-tickers.to_csv('C:/Users/baska/Downloads/NUS/Big Data/Projects/tickers.csv', encoding='utf-8')
-#print(tickers.head())
+tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+#tickers.to_csv('C:/Users/baska/Downloads/NUS/Big Data/Projects/tickers.csv', encoding='utf-8')
+print(tickers.head())
 
 # Get the data for this tickers from yahoo finance
 stock_data = yf.download(tickers.Symbol.to_list(),'2021-09-01','2022-04-01', auto_adjust=True)['Close']
-stock_data.to_csv('C:/Users/baska/Downloads/NUS/Big Data/Projects/stock_data.csv', encoding='utf-8')
-#print(data.head())
+sd = stock_data.to_csv('stock_data.csv', encoding='utf-8')
+print(data.head())
+
+
+import boto3 as boto3
+
+s3 = boto3.resource('s3',aws_access_key_id='AKIA2I4L5O2NZ532672T', aws_secret_access_key='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')
+bucket = s3.Bucket('bsaw-client-info','sd')
+result = object.put(Body=sd)
+    
+    
