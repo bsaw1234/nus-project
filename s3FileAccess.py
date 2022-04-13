3357d511e9f71f8fa695ac0815cdccf7d88f51c6
manikandan0892 <arunmani333@gmail.com>
2022-04-13 13:35:48 +0530

    Update s3FileAccess.py

--- s3FileAccess.py
+++ s3FileAccess.py
@@ -1,8 +1,16 @@
-import boto3 as boto3
+ import botocore.session
+ import boto3
 
-s3 = boto3.resource('s3',aws_access_key_id='AKIAR5SIOW5FPO6A7IFA', aws_secret_access_key='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')
-bucket = s3.Bucket('bsaw-client-info')
-for obj in bucket.objects.all():
-    key = obj.key
-    body = obj.get()['Body'].read()
-    print(body)
+def get_bucket_data(bucketname):
+   
+    session = botocore.session.get_session()
+    accesskey=session.get_credentials().access_key
+    secretkey=session.get_credentials().secret_key
+    s3 = boto3.resource('s3',aws_access_key_id=accesskey, aws_secret_access_key=secretkey)
+    bucket = s3.Bucket(bucketname)
+    for obj in bucket.objects.all():
+        key = obj.key
+        body = obj.get()['Body'].read()
+        print(body)
+
+get_bucket_data('appbkp')
