87a812d72c46c30ba6f4907c7cfcc3b4d6b44ff7
bsaw1234 <102780871+bsaw1234@users.noreply.github.com>
2022-04-14 19:06:28 +0800

    Update read_stock_data.py

--- read_stock_data.py
+++ read_stock_data.py
@@ -24,10 +24,10 @@ bucket = s3.Bucket('bsaw-client-info','tickers')
 result = object.put(Body=tickers)
 
 # Get the data for this tickers from yahoo finance and write to S3 buket
-stock_data = yf.download(tickers.Symbol.to_list(),'2021-09-01','2022-04-01', auto_adjust=True)['Close']
-sd = stock_data.to_csv('stock_data.csv', encoding='utf-8')
+df_stock_data = yf.download(tickers.Symbol.to_list(),'2021-09-01','2022-04-01', auto_adjust=True)['Close']
+stock_data = df_stock_data.to_csv('df_stock_data.csv', encoding='utf-8')
 #print(data.head())
 
 s3 = boto3.resource('s3',aws_access_key_id='AKIA2I4L5O2NZ532672T', aws_secret_access_key='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')
-bucket = s3.Bucket('bsaw-client-info','sd')
-result = object.put(Body=sd)  
+bucket = s3.Bucket('bsaw-client-info','stock_data')
+result = object.put(Body=stock_data)
