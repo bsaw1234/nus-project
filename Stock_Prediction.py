51606abe0301c898828e4878f3bc99e45b4baa94
bsaw1234 <102780871+bsaw1234@users.noreply.github.com>
2022-04-14 18:51:37 +0800

    Update Stock_Prediction.py

--- Stock_Prediction.py
+++ Stock_Prediction.py
@@ -31,7 +31,8 @@ from pmdarima import auto_arima
 #Scikit-Learn for Modeling
 from sklearn.metrics import mean_squared_error,r2_score, mean_absolute_error,mean_squared_log_error
 #df = pd.read_csv('stock_data_x.csv', sep=",")
-df = pd.read_csv('C:/Users/baska/Downloads/NUS/Big Data/Projects/stock_data_y.csv').dropna()
+#df = pd.read_csv('C:/Users/baska/Downloads/NUS/Big Data/Projects/stock_data_y.csv').dropna()
+
 l_0 = df.iloc[0].T
 df_0 = l_0.to_frame()
 df_0.columns = ["Day_0"]
@@ -61,7 +62,6 @@ for d in range(1,180):
     model_predictions_dte=pd.DataFrame(zip(ARIMA_model_new_dte),columns=(["Date"]))          
 m = model_predictions_dte
 for i in range(len(df.columns)):
-     
      tickerSymbol = df.columns[i]
      print(tickerSymbol)
      model_arima = auto_arima(model_train[tickerSymbol],trace=False, error_action='ignore', start_p=1,start_q=1,max_p=3,max_q=3,
@@ -92,35 +92,40 @@ for i in range(len(df.columns)):
          model_predictions_date=pd.DataFrame(zip(ARIMA_model_new_date),columns=(["Date"])) 
          x = pd.concat([model_predictions_date,model_predictions],axis=1)         
      m = pd.merge(m,x,left_on="Date",right_on="Date")
-     m.to_csv('C:/Users/baska/Downloads/NUS/Big Data/Projects/pred_data.csv',encoding='utf-8')
+     Predicted = m.to_csv('C:/Users/baska/Downloads/NUS/Big Data/Projects/pred_data.csv',encoding='utf-8')
+     # Write the Predicted Stock Price to S3 
+     s3 = boto3.resource('s3',aws_access_key_id='AKIA2I4L5O2NZ532672T', aws_secret_access_key='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')
+     bucket = s3.Bucket('bsaw-client-info','Predicted')
+     result = object.put(Body=Predicted)
+# Get 30 days Predicted Price        
 l_30 = m.iloc[30].T
 df_30 = l_30.to_frame()
 df_30.columns = ["Day_30"]
 df_30_fmt = df_30.reset_index().rename(columns={'index':'Symbol'})
 df_f30 = df_30_fmt.iloc[1: , :].drop('Symbol', 1)
 df_0_30 = pd.concat([df_f0,df_f30],axis=1)         
-
+# Get 60 days Predicted Price        
 l_60 = m.iloc[60].T
 df_60 = l_60.to_frame()
 df_60.columns = ["Day_60"]
 df_60_fmt = df_60.reset_index().rename(columns={'index':'Symbol'})
 df_f60 = df_60_fmt.iloc[1: , :].drop('Symbol', 1)
 df_0_30_60 = pd.concat([df_0_30,df_f60,],axis=1)
-
+# Get 90 days Predicted Price        
 l_90 = m.iloc[90].T
 df_90 = l_90.to_frame()
 df_90.columns = ["Day_90"]
 df_90_fmt = df_90.reset_index().rename(columns={'index':'Symbol'})
 df_f90 = df_90_fmt.iloc[1: , :].drop('Symbol', 1)
 df_0_30_60_90 = pd.concat([df_0_30_60,df_f90,],axis=1)
-
+# Get 120 days Predicted Price        
 l_120 = m.iloc[120].T
 df_120 = l_120.to_frame()
 df_120.columns = ["Day_120"]
 df_120_fmt = df_120.reset_index().rename(columns={'index':'Symbol'})
 df_f120 = df_120_fmt.iloc[1: , :].drop('Symbol', 1)
 df_0_30_60_90_120 = pd.concat([df_0_30_60_90,df_f120,],axis=1)
-
+# Get 180 days Predicted Price        
 l_180 = m.iloc[178].T
 df_180 = l_180.to_frame()
 df_180.columns = ["Day_180"]
@@ -128,15 +133,19 @@ df_180_fmt = df_180.reset_index().rename(columns={'index':'Symbol'})
 df_f180 = df_180_fmt.iloc[1: , :].drop('Symbol', 1)
 df_final = pd.concat([df_0_30_60_90_120,df_f180,],axis=1)
 
+# Transform as UP and Down Trend
 df_final["Trend_30"] = (np.where(df_final['Day_0'] < df_final['Day_30'], "Up", "Down"))
 df_final["Trend_90"] = (np.where(df_final['Day_0'] < df_final['Day_90'], "Up", "Down"))
 df_final["Trend_180"] = (np.where(df_final['Day_0'] < df_final['Day_180'], "Up", "Down"))
 
+# Transform as Volatility
 df_final["Trend_V_30"] = (df_final['Day_30'] - df_final['Day_0'])/(df_final['Day_0']) * 100
 df_final["Trend_V_90"] = (df_final['Day_30'] - df_final['Day_0'])/(df_final['Day_0']) * 100
 df_final["Trend_V_180"] = (df_final['Day_180'] - df_final['Day_0'])/(df_final['Day_0']) * 100
-df_final.to_csv('C:/Users/baska/Downloads/NUS/Big Data/Projects/final_data.csv',encoding='utf-8',index=False)
+Trend = df_final
 
-
-
-
+#df_final.to_csv('C:/Users/baska/Downloads/NUS/Big Data/Projects/final_data.csv',encoding='utf-8',index=False)
+# Write the Predicted Trend Stock Price to S3 
+s3 = boto3.resource('s3',aws_access_key_id='AKIA2I4L5O2NZ532672T', aws_secret_access_key='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')
+bucket = s3.Bucket('bsaw-client-info','Trend')
+result = object.put(Body=Trend)
